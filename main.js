2284e2dfb04031b440cfcb797e962c514f209774
shohan-pherones <shohan.sub.56@gmail.com>
2022-10-26 22:42:07 +0600

    xml req

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
-const dronesContainer = document.querySelector(".drones-container");
+const wrapper = document.querySelector(".country-wrapper");
+const btn = document.querySelector(".btn");
 
-fetch("https://aero-verge.herokuapp.com/dronesArr")
-  .then((response) => response.json())
-  .then((data) => getData(data));
+btn.addEventListener("click", function () {
+  const input = document.querySelector(".input-country");
+  const inputValue = input.value;
 
-function getData(drones) {
-  drones.forEach((drone) => renderData(drone));
-}
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v2/name/${inputValue}`);
+  request.send();
 
-function renderData(drone) {
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    renderData(data);
+  });
+});
+
+function renderData(country) {
+  console.log(country);
   const html = `
-  <h1>${drone.title}</h1>
-  <p>Price: $${drone.price}</p>
-`;
+  <div class="country">
+    <h1>${country.name}</h1>
+    <p>Population: ${(country.population / 10000000).toFixed(2)} M people</p>
+    <p>Capital: ${country.capital}</p>
+    <p>Language: ${country.languages[0].name}</p>
+  </div>
+  `;
 
-  dronesContainer.insertAdjacentHTML("afterbegin", html);
+  wrapper.insertAdjacentHTML("afterbegin", html);
 }
-
-fetch("https://jsonplaceholder.typicode.com/posts")
-  .then((response) => response.json())
-  .then((data) => renderPosts(data));
-
-function renderPosts(posts) {
-  posts.forEach((post) => console.log(post));
-}
-
-fetch("https://jsonplaceholder.typicode.com/comments")
-  .then((response) => response.json())
-  .then((data) => console.log(data));
